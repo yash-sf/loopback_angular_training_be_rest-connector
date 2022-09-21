70b55cdf6916fbc2eab1da64c6232ec14eec5b65
Yash Karande <yash.karande@sourcefuse.com>
2022-09-21 22:10:20 +0530

    middleware: Remove useless try catch

--- src/middlewares/create-token.middleware.ts
+++ src/middlewares/create-token.middleware.ts
@@ -4,26 +4,23 @@ const algo = 'aes256';
 
 export const createToken: Middleware = async (middlewareCtx, next) => {
   const {request} = middlewareCtx;
+
+  if (!request.headers || !request.headers.usercookie)
+    throw new Error('User cookie not found in request');
+  const userCookie: any = request.headers.usercookie;
+  let userId;
   try {
-    console.log(request.headers);
-    if (!request.headers || !request.headers.usercookie)
-      throw new Error('User cookie not found in request');
-    const userCookie: any = request.headers.usercookie;
-    let userId: number = -1;
-    try {
-      userId = JSON.parse(userCookie).userId;
-    } catch (e) {
-      throw e;
-    }
-    const jwtSecretKey: string = process.env.SECRET_KEY || 'secret-key';
-    let data = {
-      time: Date(),
-      userId: userId,
-    };
-    const token = sign(data, jwtSecretKey);
-    middlewareCtx.request.headers['authToken'] = token;
-    return await next();
+    userId = JSON.parse(userCookie).userId;
   } catch (e) {
+    console.log(e);
     throw e;
   }
+  const jwtSecretKey: string = process.env.SECRET_KEY || 'secret-key';
+  let data = {
+    time: Date(),
+    userId: userId,
+  };
+  const token = sign(data, jwtSecretKey);
+  middlewareCtx.request.headers['authToken'] = token;
+  return next();
 };
